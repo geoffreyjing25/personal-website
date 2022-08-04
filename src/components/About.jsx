4bb547e0d0e21e64273d8c1e4f2bb2fbaf2643df
Geoffrey Jing <jinggeoffrey@gmail.com>
2022-08-04 01:25:54 -0700

    updated project and several other changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,27 +45,34 @@ function About() {
         <div className='w-full md:w-6/12'>
         <SectionTitle>About Me</SectionTitle>
         <p className='text-md text-gray-600 dark:text-gray-300'>I'm an undergraduate at Carleton College passionate 
-            about computer science and currently interested in ML and Web Development. 
+            about computer science and currently interested in AI/ML, finance, entrepreneurship, and VC. 
             I'm a quick and curious learner who loves solving difficult and challenging 
             problems, and a critical thinker with a results-driven, initiative mindset. 
             I'm also a committed Division III student-athlete with extensive experience 
             in time management, leadership, and collaborative teamwork. Feel free to 
             reach out and connect with me!</p>
-            <a href="mailto:jinggeoffrey@gmail.com"
+            <br/>
+            {/* <a href="mailto:jinggeoffrey@gmail.com"
             className='block mt-3 text-md md:text-lg
              text-gray-700 dark:text-gray-300 underline hover:text-indigo-500
             dark:hover:text-indigo-500'
-            >jinggeoffrey@gmail.com</a>
+            >jinggeoffrey@gmail.com</a> */}
+            <a href="mailto: jinggeoffrey@gmail.com" className="inline-block px-6 py-3 border-transparent text-base 
+            font-medium rounded-md text-white bg-indigo-600 
+            hover:bg-indigo-700 md:text-md">
+              <button className="outline-button-sm">Get in Touch ➡️</button>
+          </a>
+                <br/>
             <br></br>
-            <p className='text-l text-gray-600 dark:text-gray-300'> Some Fun Facts:</p>
-            <p className='text-md text-gray-600 dark:text-gray-300'>Current age: <Age1/></p> 
-            <p className='text-md text-gray-600 dark:text-gray-300'>I have a twin brother. (if you're curious: <Age2/>)</p>
-            <p className='text-md text-gray-600 dark:text-gray-300'>Favorite Food: Sushi</p>
-            <p className='text-md text-gray-600 dark:text-gray-300'>College Sport: Golf </p>
-            <p className='text-md text-gray-600 dark:text-gray-300'>I once ordered 300 chicken mcnuggets for my 13th birthday.</p> 
-            <p className='text-md text-gray-600 dark:text-gray-300'> Hobbies: reading, learning new things, playing basketball, sketching/painting, working out, trying different foods</p> 
+            <p className='text-l text-gray-600 dark:text-gray-300'>Fun Stats About Me:</p>
+            <p className='text-md text-gray-600 dark:text-gray-300'>Current age: <Age1/></p>
+            <p className='text-md text-gray-600 dark:text-gray-300'>Current Typing Speed: 125 wpm</p>
+            <p className='text-md text-gray-600 dark:text-gray-300'>I have a brother who is 1 minute older (identical twin 😉)</p>
+            {/* <p className='text-md text-gray-600 dark:text-gray-300'>Favorite Food: Sushi</p> */}
+            <p className='text-md text-gray-600 dark:text-gray-300'>Most chicken nuggets ordered at once: 300 for my 13th birthday</p> 
+            {/* <p className='text-md text-gray-600 dark:text-gray-300'> Hobbies: reading, learning new things, playing basketball, sketching/painting, working out, trying different foods</p>  */}
         </div>
-        <img src="https://avatars.githubusercontent.com/u/96399544?v=4" 
+        <img src="http://drive.google.com/uc?export=view&id=1RFg0RarI8Sm1wbH1VNJw6lTTqqOuXLQt"
         alt="Geoffrey Jing" className='w-full md:w-6/12 rounded-lg object-cover'/>
     </div>
   )
--- src/data/works.js
+++ src/data/works.js
@@ -8,30 +8,28 @@ export default [
 		title: 'InvestED: Educational Investment Simulator Web Application',
 		imgUrl:
 			'https://investmentu.com/wp-content/uploads/2020/03/which-stocks-should-i-invest-in.jpg',
-		tech: ['HTML/CSS', 'JavaScript', 'SQL', 'PostgreSQL', 'pandas', 'Matplotlib'],
+		tech: ['Flask', 'HTML/CSS', 'JavaScript', 'SQL', 'PostgreSQL', 'pandas', 'Matplotlib'],
         workUrl: 'https://github.com/CS-257-S22/H',
+	},
+	{
+		title: 'Todo App',
+		imgUrl:
+		'https://i.stack.imgur.com/thsNp.jpg',
+		tech: ['Next.js', 'styled-components', 'useForm', 'useState', 'JavaScript', 'HTML'],
+		workUrl: 'https://github.com/geoffreyjing25/todo-app',
+	},
+	{
+		title: "Personal Website",
+		imgUrl:
+		'http://drive.google.com/uc?id=1mVY9uB0qYU3aBVoJAlOqKp7xScCJ9bDd',
+		tech: ['React.js', 'Tailwind CSS', 'JavaScript', 'HTML', 'CSS'],
+        workUrl: 'https://github.com/geoffreyjing25/personal-website',
 	},
 	{
 		title: "Carleton Robotics: Autonomous Electric Vehicle",
 		imgUrl:
 			'https://blog.arduino.cc/wp-content/uploads/2016/08/7731171470230745796.jpg',
 		tech: ['Python', 'C++', 'Arduino'],
-        workUrl: 'https://github.com/Carleton-Robotics/Arduino-Kart',
-	},
-	{
-		title: "Personal Website",
-		imgUrl:
-			'http://drive.google.com/uc?id=1mVY9uB0qYU3aBVoJAlOqKp7xScCJ9bDd',
-		tech: ['React.js', 'Tailwind CSS', 'JavaScript', 'HTML', 'CSS'],
-        workUrl: 'https://github.com/geoffreyjing25/personal-website',
-	},
-	{
-		title: 'Silver Dollar Coin Strip Game',
-		imgUrl:
-			// 'https://raw.githubusercontent.com/ShaifArfan/artistic/main/readmeImg/banner.png',
-			// 'https://images.twinkl.co.uk/tw1n/image/private/t_630/image_repo/b6/d0/au-n-419-australian-coins-strip-number-cards-_ver_1.jpg',
-			'https://nfb.org/sites/default/files/image-content/resources/Louis-Braille-Silver-Dollar-Proof.jpg',
-		tech: ['Java'],
-        workUrl: 'https://github.com/geoffreyjing25/CoinStrip-Game',
+		workUrl: 'https://github.com/Carleton-Robotics/Arduino-Kart',
 	},
 ];
